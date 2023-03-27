1b2925d3dfeca27235c87a2862776ca7577228b0
madelinegannon <madeline.gannon@gmail.com>
2023-03-27 15:18:45 -0400

    refactored to example-clearpath-helloworld

---
+++ myApps/example-clearpath-helloworld/src/ofApp.cpp
@@ -0,0 +1,125 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetLogLevel(OF_LOG_NOTICE);
+	ofLogToConsole();
+
+	if (!initialize()) {
+		exit();	// <-- not killing the program for some reason
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	ofBackground(60);
+}
+
+//--------------------------------------------------------------
+/**
+ * @brief Create the System Manger, then setup and open the port.
+ * Logs how many nodes it finds on the port.
+ *
+ * @return (bool) True if the system was initialized properly.
+ *				  False if there was a problem accessing the port.
+ *
+ */
+bool ofApp::initialize() {
+
+	// Create the System Manager 
+	myMgr = SysManager::Instance();
+
+	int portCount = 0;
+	vector<string> comHubPorts;
+	try {
+		SysManager::FindComHubPorts(comHubPorts);
+		ofLogNotice("ofApp::initialize") << "Found " << ofToString(comHubPorts.size()) << " SC Hubs";
+
+		// Define the first SC Hub port (port 0) to be associated with COM portnum (as seen in windows device manager)
+		for (portCount = 0; portCount < comHubPorts.size() && portCount < NET_CONTROLLER_MAX; portCount++) {
+			myMgr->ComHubPort(portCount, comHubPorts[portCount].c_str());
+		}
+
+		if (portCount > 0) {
+			// Open the port
+			myMgr->PortsOpen(portCount);
+
+			for (size_t i = 0; i < portCount; i++) {
+				IPort& myPort = myMgr->Ports(i);
+				ofLogNotice("ofApp::initialize") << "\tSTATUS: Port " << myPort.NetNumber() << ", state=" << myPort.OpenState() << ", nodes=" << myPort.NodeCount();
+
+			}
+		}
+		else {
+			ofLogWarning("ofApp::initialize") << "Unable to locate SC hub port";
+			return false;
+		}
+		return true;
+	}
+	catch (mnErr& theErr)
+	{
+		ofLogError("ofApp::initialize") << "Port Failed to open, Check to ensure correct Port number and that ClearView is not using the Port";
+		ofLogError("ofApp::initialize") << "Caught error: addr=" << ofToString(theErr.TheAddr) << ", err=" << ofToString(theErr.ErrorCode) << ", msg=" << ofToString(theErr.ErrorMsg);
+		return false;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
